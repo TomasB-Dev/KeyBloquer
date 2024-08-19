a36eaea4691734a99016a08bdd5ac29bf70694aa
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2024-08-18 22:54:25 -0300

    fix ui

--- KeyBlocker.py
+++ KeyBlocker.py
@@ -7,6 +7,7 @@ from PIL import Image, ImageDraw
 Estado_de_key = False
 tecla_a_bloquear = None
 
+
 def Desbloquear_Key():
     global Estado_de_key  
     if (tecla_a_bloquear != None):
@@ -41,6 +42,9 @@ def seleccionar_tecla(tecla):
     global tecla_a_bloquear
     tecla_a_bloquear = tecla
     etiqueta_tecla.config(text=f"TECLA SELECCIONADA: {tecla}")
+    actualizar_botones()
+
+
 def minimize_to_tray():
     root.withdraw()  # oculto la ventna
     icon.run()
@@ -62,15 +66,20 @@ def create_tray_icon():
     )
     
     return Icon("KeyBlocker", image, "KeyBlocker", menu)
+
+
 #capta los eventos del teclado
 keyboard.hook(Tecla_Pulsada)
+#actualiza el color de la tecla seleccionada
+def actualizar_botones():
+    for tecla, boton in botones.items():
+        if tecla == tecla_a_bloquear:
+            boton.config(bg="ORANGE")
+        else:
+            boton.config(bg="GREY")
 root = tk.Tk()
 root.title("KeyBlocker")
 root.config(bg="#272623")
-
-
-
-
 
 etiqueta_tecla = tk.Label(root, text="TECLA SELECCIONADA: NINGUNA", font=("Arial", 14))
 etiqueta_tecla.grid(row=0, column=0, columnspan=14, pady=10)
@@ -81,16 +90,18 @@ teclas = [
     'shift','<','z','x','c','v','b','n','m',',','.','-','shif left','ctrl','command','alt','spacebar','alt gr','Function',
     'Insert','print screen',
 
-
 ]
+botones = {}
 Cantidad_De_Teclas = 0
 for tecla in teclas:
     row = (Cantidad_De_Teclas // 14) + 1  
-    column = Cantidad_De_Teclas % 14  
-    boton = tk.Button(root, text=f"{tecla.upper()}", command=lambda t=tecla: seleccionar_tecla(t))
-    boton.grid(row=row + 1, column=column, padx=5, pady=5)  #
-    
+    column = Cantidad_De_Teclas % 14 
+    bck = "ORANGE" if tecla == tecla_a_bloquear else "GREY"
+    boton = tk.Button(root, text=f"{tecla.upper()}", command=lambda t=tecla: seleccionar_tecla(t), bg=bck)
+    boton.grid(row=row + 1, column=column, padx=5, pady=5)
+    botones[tecla] = boton  
     Cantidad_De_Teclas += 1
+
 
 boton_bloquear = tk.Button(root, text="Bloquear tecla", command=Bloquear_Key, bg="red")  
 boton_bloquear.grid(row=1, column=0, columnspan=7, pady=20, sticky="we")
